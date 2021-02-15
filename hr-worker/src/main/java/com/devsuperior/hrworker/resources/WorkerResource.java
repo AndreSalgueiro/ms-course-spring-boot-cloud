d01d50388a2eb85252d48f00eb0a6a3e4fe0cd93
AndreSalgueiro <andrecoordena@hotmail.com>
2021-02-15 08:48:07 -0300

    Peivate Git repository

--- hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/resources/WorkerResource.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,12 +21,15 @@ import com.devsuperior.hrworker.repositories.WorkerRepository;
 @RefreshScope
 @RestController
 @RequestMapping(value = "/workers")
+//@PropertySource("classpath:hr-config-server/application.properties")
 public class WorkerResource {
 	
 	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 	
-	@Value("${test.config}")
+	
+	@Value("${test.config}") 
 	private String testConfig;
+	 
 	
 	@Autowired
 	private Environment envi;
@@ -37,6 +41,7 @@ public class WorkerResource {
 	@GetMapping(value = "/configs")
 	public ResponseEntity<Void> getConfigs(){
 		logger.info("CONFIG = " + testConfig);
+		//logger.info("CONFIG = " + envi.getProperty("test.config"));
 		return ResponseEntity.noContent().build();
 	}
 	
